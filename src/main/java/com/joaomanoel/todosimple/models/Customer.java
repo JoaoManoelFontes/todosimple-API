adca18e02695b2e4186811567c84a11b9e29f0bd
JoaoManoelFontes <jmanufontesc@gmail.com>
2024-09-06 08:20:53 -0300

    feat: add customer model

---
+++ src/main/java/com/joaomanoel/todosimple/models/Customer.java
@@ -0,0 +1,59 @@
+package com.joaomanoel.todosimple.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = Customer.TABLE_NAME)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    public interface CreateCustomer {
+    }
+
+    ;
+
+    public interface UpdateCustomer {
+    }
+
+    ;
+    public static final String TABLE_NAME = "customer";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true)
+    private UUID id;
+    @Column()
+    @NotBlank(groups = {CreateCustomer.class}, message = "O username não deve ser vazio.")
+    @Min(value = 4, groups = {CreateCustomer.class}, message = "O username deve ter no mínimo 4 caractéres.")
+    @Max(value = 50, groups = {CreateCustomer.class}, message = "O username deve ter no máximo 50 caractéres.")
+    private String username;
+    @Column(unique = true)
+    @Email(groups = {CreateCustomer.class}, message = "O Email deve ser válido.")
+    private String email;
+    @Column()
+    @NotBlank(groups = {CreateCustomer.class, UpdateCustomer.class}, message = "A senha não deve ser vazia.")
+    @Min(value = 8, groups = {CreateCustomer.class, UpdateCustomer.class}, message = "A senha deve ter no mínimo 4 caractéres.")
+    @Max(value = 50, groups = {CreateCustomer.class, UpdateCustomer.class}, message = "A senha deve ter no máximo 50 caractéres.")
+    private String password;
+//  private List<Task> tasks = new ArrayList<Task>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        return Objects.equals(id, customer.id) && Objects.equals(username, customer.username) && Objects.equals(email, customer.email) && Objects.equals(password, customer.password);
+    }
+}
